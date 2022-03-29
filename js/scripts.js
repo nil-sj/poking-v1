69bb649d17d7a1b7bb179975dcd110ddc104f121
Nil SJ <niladri123@gmail.com>
2022-03-29 10:49:55 -0400

    detect devices OS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+var Name = "Not known";
+        if (navigator.appVersion.indexOf("Win") != -1) Name =
+          "Windows OS";
+        if (navigator.appVersion.indexOf("Mac") != -1) Name =
+          "MacOS";
+        if (navigator.appVersion.indexOf("X11") != -1) Name =
+          "UNIX OS";
+        if (navigator.appVersion.indexOf("Linux") != -1) Name =
+          "Linux OS";
+
+document.getElementById("details").textContent = Name;
+
+
 const details = navigator.userAgent;
 
 // Creating a regular expression containing some mobile devices keywords
